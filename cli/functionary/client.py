1146d5122f9babf6884f9ec777ca0387c7f7c347
catattack05 <105679648+catattack05@users.noreply.github.com>
2022-10-13 15:41:45 -0400

    #66 - CLI: Improve 400 error handling (#101)

--- cli/functionary/client.py
+++ cli/functionary/client.py
@@ -1,5 +1,3 @@
-import json
-
 import click
 import requests
 
@@ -18,7 +16,7 @@ def get(endpoint):
 
     """
     response = _send_request(endpoint, "get")
-    response_data = json.loads(response.text)
+    response_data = response.json()
 
     if "results" in response_data:
         return response_data["results"]
@@ -40,7 +38,40 @@ def post(endpoint, data=None, files=None):
 
     """
     response = _send_request(endpoint, "post", post_data=data, post_files=files)
-    return json.loads(response.text)
+    return response.json()
+
+
+def _400_error_handling(response):
+    """
+    Helper function for _send_request that gives more user friendly
+    responses from 400 error codes
+
+    Args:
+        response: Python response object with 400 error
+    Raises:
+        ClickException with user friendly message based on response
+    """
+    message = None
+    code = response.json()["code"]
+
+    match code:
+        case "missing_env_header":
+            message = (
+                "No environment selected. Please set environment id "
+                + "using 'functionary package environment set'."
+            )
+        case "invalid_env_header":
+            message = (
+                "Invalid environment provided. Please set environment "
+                + "using 'functionary package environment set'."
+            )
+        case "invalid_teamid_header":
+            message = "Team ID is invalid."
+        case "invalid_package":
+            message = f"{response.json()['detail']}"
+    if message is None:
+        message = f"{response.json()['detail']}"
+    raise click.ClickException(message)
 
 
 def _send_request(endpoint, request_type, post_data=None, post_files=None):
@@ -91,11 +122,7 @@ def _send_request(endpoint, request_type, post_data=None, post_files=None):
     if response.ok:
         return response
     elif response.status_code == 400:
-        # TODO: Add error codes checking in addition to status once they are added
-        # to the API
-        raise click.ClickException(
-            "Please set an active environment id using 'environment set'"
-        )
+        _400_error_handling(response)
     elif response.status_code == 401:
         raise click.ClickException("Authentication failed. Please login and try again.")
     elif response.status_code == 403:
