046fef009cc15a68608457bfed63bfc03b113b98
Scott Taubman <86254668+scott-taubman@users.noreply.github.com>
2023-02-21 19:36:52 -0500

    Added parameter_types example function (#226)

--- examples/demo/functions.py
+++ examples/demo/functions.py
@@ -1,5 +1,6 @@
 import os
 import time
+from datetime import date, datetime
 
 import requests
 
@@ -38,3 +39,31 @@ def num_chars(file: str) -> int:
     if request.status_code == 200:
         return len(request.text)
     return 0
+
+
+def parameter_types(
+    boolean_: bool | None = None,
+    date_: date | None = None,
+    datetime_: datetime | None = None,
+    file_: str | None = None,
+    float_: float | None = None,
+    integer_: int | None = None,
+    json_: dict | None = None,
+    string_: str | None = None,
+    text_: str | None = None,
+):
+    return [
+        {"parameter": "boolean_", "type": type(boolean_).__name__, "value": boolean_},
+        {"parameter": "date_", "type": type(date_).__name__, "value": date_},
+        {
+            "parameter": "datetime_",
+            "type": type(datetime_).__name__,
+            "value": datetime_,
+        },
+        {"parameter": "file_", "type": type(file_).__name__, "value": file_},
+        {"parameter": "float_", "type": type(float_).__name__, "value": float_},
+        {"parameter": "integer_", "type": type(integer_).__name__, "value": integer_},
+        {"parameter": "json_", "type": type(json_).__name__, "value": json_},
+        {"parameter": "string_", "type": type(string_).__name__, "value": string_},
+        {"parameter": "text_", "type": type(text_).__name__, "value": text_},
+    ]
