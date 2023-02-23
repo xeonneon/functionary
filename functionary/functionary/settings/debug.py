bd67f9d3ca0caff01e5e1eb2db673d1fc97ee528
Kevin Vig <kvig@geontech.com>
2023-02-23 15:58:25 -0500

    Import local.py for additional settings if present (#228)

--- functionary/functionary/settings/debug.py
+++ functionary/functionary/settings/debug.py
@@ -18,3 +18,5 @@ INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + [
     "127.0.0.1",
     "10.0.2.2",
 ]
+
+from .local_settings import *  # noqa
---
+++ functionary/functionary/settings/local_settings.py
@@ -0,0 +1,5 @@
+from .base import *  # noqa
+
+# Add overrides here like:
+# INSTALLED_APPS += ["django_watchfiles"]
+# MY_SETTINGS_VAR = "new_value"
